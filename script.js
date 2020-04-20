fc72af843b015f23a7352eace7c1beb302dd9020
Samriddhi Jain <samriddhijain2000@gmail.com>
2020-04-20 19:51:13 +0530

    update

--- script.js
+++ script.js
@@ -7,18 +7,42 @@ const btn = document.getElementById('button');
 
 btn.addEventListener('click', (e)=>{
 
-	if(name.value===''||pass.value===''||pass_2.value===''||name.value==null||pass.value==null||pass_2.value==null){
+	if(name.value===''||name.value==null){
 		e.preventDefault();
-		error.innerText= 'Please fill in all the fields.';
+		error.innerText = 'Please fill in Username';
+		name.classList.add('err');
+
+		setTimeout(()=> name.classList.remove('err'),2000);
+	}
+	else if(pass.value===''||pass.value==null){
+		e.preventDefault();
+		error.innerText = 'Please fill in Password';
+		pass.classList.add('err');
+
+		setTimeout(()=> pass.classList.remove('err'),2000);
+	}
+	else if(pass_2.value===''||pass_2.value==null){
+		e.preventDefault();
+		error.innerText = 'Please fill in Confirm Password';
+		pass_2.classList.add('err');
+
+		setTimeout(()=> pass_2.classList.remove('err'),2000);
 	}
 
 	else if(pass.value.length!==8){
 		e.preventDefault();
 		error.innerText = 'Password should be 8 characters long!';
+		pass.classList.add('err');
+
+		setTimeout(()=> pass.classList.remove('err'),2000);
+
 	}
 	else if(pass.value !== pass_2.value){
 		e.preventDefault();
 		error.innerText = 'Passwords must match!!';
+		pass_2.classList.add('err');
+
+		setTimeout(()=> pass_2.classList.remove('err'),2000);
 	}
 
 	else{
@@ -26,3 +50,9 @@ btn.addEventListener('click', (e)=>{
 	}
 
 });
+
+
+// function removeError(a){
+// 	error.remove();
+// 	a.classList.remove('err');
+// }
